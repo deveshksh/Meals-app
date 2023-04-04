c30a6d663f6e5f5a7d24cc8e32d0d7e8260f1fe1
Devarshi <devdubey7582@gmail.com>
2023-04-04 16:41:57 +0530

    added cors headers

--- config/settings.py
+++ config/settings.py
@@ -39,17 +39,26 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "apis",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+"http://localhost:3500",
+"http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3500"] 
 
 ROOT_URLCONF = 'config.urls'
 
